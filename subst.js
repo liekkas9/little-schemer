0a2a4e97a267ad1ed69650c44a0a2dd5cd07d8a0
lingxufeng <lingxufeng@bytedance.com>
2020-01-04 23:10:40 +0800

    subst 虽然有一些 bug,但是成了

---
+++ subst.js
@@ -0,0 +1,29 @@
+// 递归替换掉全部单词
+function subst(newVal = '', oldVal = '', arr = []) {
+	if (!arr.length) {
+		return arr;
+	} else {
+		const [first, ...rest] = arr;
+		if (typeof first === 'string') {
+			if (first === oldVal) {
+				return [newVal, ...subst(newVal, oldVal, rest)];
+			} else {
+				return [first, ...subst(newVal, oldVal, rest)];
+			}
+		} else {
+			return [subst(newVal, oldVal, first), subst(newVal, oldVal, rest)];
+		}
+	}
+}
+
+const testData = [
+	['banana'],
+	['split', [['banana', 'ice'], ['create', 'banana'], 'sherbet']],
+	['banana'],
+	['bread'],
+	['banana', 'brandy']
+];
+
+const ret = subst('orange', 'banana', testData);
+console.log(ret);
+console.log(JSON.stringify(ret, null, 2));
