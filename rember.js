f33acca8d43813a07c97e1ca111a2427ed693d74
lingxufeng <lingxufeng@bytedance.com>
2020-01-05 13:25:11 +0800

    udpate rember.ks

--- rember.js
+++ rember.js
@@ -1,17 +1,21 @@
 function rember(a = '', lat = []) {
 	if (!lat.length) {
-		return lat;
+		return [];
 	} else {
 		const [first, ...rest] = lat;
-		if (first === a) {
-			return rest;
+		if (typeof first === 'string') {
+			if (first === a) {
+				return rember(a, rest);
+			} else {
+				return [first, rember(a, rest)];
+			}
 		} else {
-			return [first, ...rember(a, rest)];
+			return [...rember(a, first), ...rember(a, rest)];
 		}
 	}
 }
 
 module.exports = rember;
 
-const ret = rember('a', ['b']);
+const ret = rember('a', ['b', ['a']]);
 console.log(ret);
